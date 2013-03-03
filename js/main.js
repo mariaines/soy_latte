012508e6c8bcdd8695dfe4aa00ad88080a132afb
Maria-Ines <mrnscrrr@gmail.com>
2013-03-02 21:53:42 -0800

    display words on screen at random positions1 second apart

--- js/main.js
+++ js/main.js
@@ -4,9 +4,18 @@ $(document).ready(function() {
   var words = text.split(" ");
   var i=0;
   var interval = setInterval(function(){
-      canvas.text(words[i++]);
+      addWord(words[i++]);
       if (i==words.length) {
         window.clearInterval(interval)
       }
     }, 1000);
 });
+
+function addWord(word) {
+  var d = document.createElement('div');
+  $(d).addClass("word")
+      .html(word)
+      .appendTo(canvas)
+      .css("top", Math.random()*400 + "px")
+      .css("left", Math.random()*700 + "px");
+}
